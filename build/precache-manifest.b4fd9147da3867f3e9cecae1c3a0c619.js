5e3337c9b31c48fe51b509c2505fcaa150ceacec
Abashev Nikolay <n.abashev@ente-ltd.ru>
2023-08-02 17:04:28 +0500

    forget build folder

---
+++ build/precache-manifest.b4fd9147da3867f3e9cecae1c3a0c619.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cd82668f864f3249962",
+    "url": "./static/css/main.adf6a368.chunk.css"
+  },
+  {
+    "revision": "4cd82668f864f3249962",
+    "url": "./static/js/main.4cd82668.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "db1cbe7ca671d52cb897",
+    "url": "./static/css/2.f6ca0682.chunk.css"
+  },
+  {
+    "revision": "db1cbe7ca671d52cb897",
+    "url": "./static/js/2.db1cbe7c.chunk.js"
+  },
+  {
+    "revision": "8780baa94f012a68296a99d14e2eab83",
+    "url": "./static/media/DiplomS.8780baa9.jpg"
+  },
+  {
+    "revision": "970d9df3d7156a84574dfa97d5b2e612",
+    "url": "./static/media/DiplomM.970d9df3.jpg"
+  },
+  {
+    "revision": "2b162f2585d100d2a871c5c412be2108",
+    "url": "./static/media/DiplomV.2b162f25.png"
+  },
+  {
+    "revision": "6021b9f6d8a251c4c478439e4541d730",
+    "url": "./static/media/Chemodan.6021b9f6.jpeg"
+  },
+  {
+    "revision": "664174a8eeb972e63db463435cc9d745",
+    "url": "./static/media/Cars.664174a8.png"
+  },
+  {
+    "revision": "4628cbec596056e306ddf81622a61d0a",
+    "url": "./static/media/Robot.4628cbec.jpeg"
+  },
+  {
+    "revision": "805683eb03424addb2a6422e4df3f36c",
+    "url": "./static/media/logo-dark.805683eb.svg"
+  },
+  {
+    "revision": "25364c81ac3e5c005e7464c00a136cfc",
+    "url": "./static/media/agp.25364c81.png"
+  },
+  {
+    "revision": "e5bc2a6e3f2b2c71dfe9d5c01f25a41f",
+    "url": "./static/media/gaz-kirg.e5bc2a6e.png"
+  },
+  {
+    "revision": "51ab96694a167445590245c35fb3d9f8",
+    "url": "./static/media/gazoraspredelenie.51ab9669.png"
+  },
+  {
+    "revision": "a294c03ce279cd3923589ac7add23c47",
+    "url": "./static/media/gt-krasnodar.a294c03c.png"
+  },
+  {
+    "revision": "46d3ac5518da612040ae9555f863c761",
+    "url": "./static/media/gt-kazan.46d3ac55.png"
+  },
+  {
+    "revision": "bd1dbd22f07a487b7e0d061f977f6a5a",
+    "url": "./static/media/gt-stavropol.bd1dbd22.png"
+  },
+  {
+    "revision": "afd79236876cde633957c96739b3da57",
+    "url": "./static/media/gt-uhta.afd79236.png"
+  },
+  {
+    "revision": "5f48d7500be1553142cc4c630d16f593",
+    "url": "./static/media/gt-volgograd.5f48d750.png"
+  },
+  {
+    "revision": "f5e66ed582d52e67c80d722f1a66e7b3",
+    "url": "./static/media/gt-yugorsk.f5e66ed5.png"
+  },
+  {
+    "revision": "c0e831ac0edd4689461170762fb26f10",
+    "url": "./static/media/gt_piter.c0e831ac.png"
+  },
+  {
+    "revision": "8741ef8cca7eea5cae21651a0583bbee",
+    "url": "./static/media/gt_chaik.8741ef8c.png"
+  },
+  {
+    "revision": "958f89eafcb49eb5b2cdb8dfbfb8c19e",
+    "url": "./static/media/lukioil.958f89ea.png"
+  },
+  {
+    "revision": "fb8d323c22e8c8e2d67c33da3f2be4c8",
+    "url": "./static/media/stream.fb8d323c.png"
+  },
+  {
+    "revision": "0b9f8cee7727ba6a9ba72e3858e830d4",
+    "url": "./static/media/hallibuton.0b9f8cee.png"
+  },
+  {
+    "revision": "18c2503db9a222b5f2cec104866b9aec",
+    "url": "./static/media/ttg.18c2503d.png"
+  },
+  {
+    "revision": "dfb714a25b3faaa869c3d6f6c95d3420",
+    "url": "./static/media/trans.dfb714a2.png"
+  },
+  {
+    "revision": "abea3c873e9497dead17bfb82a2e5926",
+    "url": "./static/media/NordStream.abea3c87.png"
+  },
+  {
+    "revision": "47f6ce5d046708c5bf5707bc719f5adb",
+    "url": "./static/media/Employees.47f6ce5d.svg"
+  },
+  {
+    "revision": "0701745add097d7deb8b4a8cf723dcc4",
+    "url": "./static/media/Foundation.0701745a.svg"
+  },
+  {
+    "revision": "e0a56cc56596e63f7fbec7c4268826a3",
+    "url": "./static/media/Introscan.e0a56cc5.svg"
+  },
+  {
+    "revision": "cd1da9810b319c024ef6654a5861a7b1",
+    "url": "./static/media/World.cd1da981.svg"
+  },
+  {
+    "revision": "dcc1fb3eed9825df581d0fba73a54549",
+    "url": "./static/media/Customers.dcc1fb3e.svg"
+  },
+  {
+    "revision": "9d74a2e46a445549e1ed1d4bfc72a778",
+    "url": "./static/media/DigitalWorld.9d74a2e4.jpg"
+  },
+  {
+    "revision": "3e604e2521e043655f879d556647b60c",
+    "url": "./index.html"
+  }
+];
